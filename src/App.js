b3dd2560c48762bc706607ddd118a38a5bda6571
huy227 <huy2272@gmail.com>
2022-08-22 23:46:35 -0400

    Created a custom component with styling
    ExpenseItem displays the name, date and price of the expense.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+        <h2>This is also visible</h2>
+        <ExpenseItem></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
