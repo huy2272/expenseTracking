fdb75cc3cdd1f75b1c5e9a9176760b3c302d27b8
huy227 <huy2272@gmail.com>
2022-08-23 21:39:38 -0400

    Dynamically setting values to expenseItem using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem(props) {
     //Can only return one root element per return statement.
     return (
         <div className="expense-item">
-            <div>22/08/2022</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$300.00</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
