26d4809a8225d49c7da5bdbcfe4658dcf6bf9ed7
huy227 <huy2272@gmail.com>
2022-09-02 19:57:29 -0400

    Making the 'Add Expense' functionality
    Users can now add expense and it will be added into the array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,26 +13,16 @@ function Expenses(props) {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date}
+                {props.items.map((expense) => (
+                <ExpenseItem 
+                //By adding a key we avoid bugs 
+                //and React will not have to go through each element in arr
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount}
+                date={expense.date}
                 />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date}
-                />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date}
-                />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date}
-                />
+                ))}
             </Card>
         </div>
 
