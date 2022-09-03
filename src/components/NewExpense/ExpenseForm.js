abcf0012cca6c2fba7cf71d661c2e44191ca492e
huy227 <huy2272@gmail.com>
2022-09-03 13:46:23 -0400

    'Add New Expense' form is now rendered conditionally

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -90,6 +90,7 @@ function ExpenseForm(props) {
                 min="2019-01-01" max="2022-12-31" />
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </div>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import React, { useState } from 'react'
 
 function NewExpense(props) {
+    const [isEditing, setIsediting] = useState(false);
 
     function saveExpenseDataHandler(enteredExpenseData) {
         const expenseData = {
@@ -9,10 +11,24 @@ function NewExpense(props) {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsediting(false);
+    }
+
+    function startEditingHandler() {
+        setIsediting(true);
+    }
+
+    function stopEditingHandler(){
+        setIsediting(false);
     }
 
     return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && 
+        <ExpenseForm 
+        onCancel={stopEditingHandler} 
+        onSaveExpenseData={saveExpenseDataHandler} 
+        />}
     </div>
 }
 export default NewExpense;
